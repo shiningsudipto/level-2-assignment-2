39e039ced3ab347c4e31f79052f427e001b2c29b
Sudipta Das <shiningsudipto@gmail.com>
2024-05-21 03:25:56 +0600

    interface and model created

---
+++ src/app/product/product.model.ts
@@ -0,0 +1,27 @@
+import { Schema, model } from 'mongoose'
+import { TInventory, TProduct, TVariant } from './product.interface'
+
+// Variant schema
+const variantSchema = new Schema<TVariant>({
+  type: { type: String, required: true },
+  value: { type: String, required: true },
+})
+
+// Inventory schema
+const inventorySchema = new Schema<TInventory>({
+  quantity: { type: Number, required: true },
+  inStock: { type: Boolean, required: true },
+})
+
+// Product schema
+const productSchema = new Schema<TProduct>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  tags: { type: [String], required: true },
+  variants: { type: [variantSchema], required: true },
+  inventory: { type: inventorySchema, required: true },
+})
+
+export const ProductModel = model<TProduct>('Product', productSchema)
