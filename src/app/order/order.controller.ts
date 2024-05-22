d59e92bd89cb1a4b2d55f9d9bf180250cf3692d6
Sudipta Das <shiningsudipto@gmail.com>
2024-05-22 13:01:27 +0600

    order validation added

--- src/app/order/order.controller.ts
+++ src/app/order/order.controller.ts
@@ -21,11 +21,11 @@ const createOrder = async (req: Request, res: Response) => {
       message: 'Order created successfully!',
       data: result,
     })
-  } catch (error) {
+  } catch (error: any) {
     res.status(500).json({
       success: false,
       message: 'Something went wrong',
-      error: error,
+      error: error.message,
     })
   }
 }
@@ -34,23 +34,36 @@ const getAllOrders = async (req: Request, res: Response) => {
   try {
     const { email } = req.query
     const result = await OrderServices.getAllOrdersFromDb(email as string)
-    if (email && result) {
+
+    if (!result || result.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: 'Order not found',
+      })
+    }
+
+    const { error, value } = orderValidationSchema.validate(result)
+
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        message: 'Validation error',
+        error: error.details,
+      })
+    }
+    if (email) {
       return res.status(200).json({
         success: true,
         message: 'Orders fetched successfully for user email!',
-        data: result,
+        data: value,
       })
-    } else if (!email && result) {
+    } else {
       return res.status(200).json({
         success: true,
         message: 'Orders fetched successfully!',
-        data: result,
+        data: value,
       })
     }
-    return res.status(404).json({
-      success: false,
-      message: 'Order not found',
-    })
   } catch (error: any) {
     return res.status(500).json({
       success: false,
